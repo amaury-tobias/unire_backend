dd0b03b74116d8739875c48dd11de44a96a5d4da
Amaury-Tobias@CSI <amaury.tobiasqr+csi@gmail.com>
2019-04-30 14:56:19 -0500

    Removed unecesary dependencies

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
-const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const passport = require('./passport')
@@ -12,19 +11,14 @@ var usuariosRouter = require('./routes/usuarios')
 
 var app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', noticiasRouter)
 app.use('/', passport.authenticate('jwt', { session: false }), indexRouter)
-app.use('/api', usuariosRouter)
+app.use('/api',passport.authenticate('jwt', { session: false }), usuariosRouter)
 
 app.use(function(req, res, next) {
   next(createError(404))
